a1b1861f127e817a10633c005fe20e3c9a65755f
sravanba1992 <sravanba1992@gmail.com>
2022-09-24 20:57:11 +0530

    added database code

--- mainapp.py
+++ mainapp.py
@@ -1,21 +1,22 @@
 from flask import Flask,render_template, request
-#from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 
 
 app =Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:root@127.0.0.1/mydb'
-#db=SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://root:root@127.0.0.1/datacollector'
+db=SQLAlchemy(app)
 
-# class Data(db.Model):
-#     __tablename__="data"
-#     id=db.Column(db.Integer, primary_key=True)
-#     email=db.Column(db.String(120),unique=True)
-#     height=db.Column(db.Integer)
-#
-#     def __init__(self, email_,height_):
-#         self.height_ = height_
-#         self.email_=email_
+class Data(db.Model):
+    """"This is base class """
+    __tablename__="data"
+    id=db.Column(db.Integer, primary_key=True)
+    email=db.Column(db.String(120),unique=True)
+    height=db.Column(db.Integer)
+
+    def __init__(self, email_,height_):
+        self.height = height_
+        self.email=email_
         
 
 
@@ -26,8 +27,11 @@ def index():
 @app.route("/success", methods=['POST'])
 def success_method():
     if request.method=='POST':
-        email = request.form['email_name']
-        height = request.form['height_name']
+        email = request.form["email_name"]
+        height = request.form["height_name"]
+        data = Data(email,height)
+        db.session.add(data)
+        db.session.commit()
         return render_template("success.html")
 
 if __name__=="__main__":
